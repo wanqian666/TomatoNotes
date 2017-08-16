efaa9c7a2bfe9ce9793a741d6cc96a154dbde43f
wanqian666 <qwe9812321qwe@sina.com>
2017-08-16 11:38:05 +0800

    2

---
+++ 04.代码/TomatoNotes/Notes/FormMain.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Notes
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Alarm = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Detail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EndTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Finish = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xinjian = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Num,
+            this.Alarm,
+            this.Detail,
+            this.EndTime,
+            this.Finish});
+            this.dataGridView1.Location = new System.Drawing.Point(-41, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 251);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
+            this.dataGridView1.CellMouseLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseLeave);
+            this.dataGridView1.CellMouseMove += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseMove);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // Num
+            // 
+            this.Num.HeaderText = "No.";
+            this.Num.Name = "Num";
+            this.Num.ReadOnly = true;
+            this.Num.Width = 30;
+            // 
+            // Alarm
+            // 
+            this.Alarm.HeaderText = "";
+            this.Alarm.Name = "Alarm";
+            this.Alarm.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Alarm.Width = 50;
+            // 
+            // Detail
+            // 
+            this.Detail.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Detail.HeaderText = "事项";
+            this.Detail.Name = "Detail";
+            // 
+            // EndTime
+            // 
+            this.EndTime.HeaderText = "预定";
+            this.EndTime.Name = "EndTime";
+            // 
+            // Finish
+            // 
+            this.Finish.HeaderText = "";
+            this.Finish.Name = "Finish";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column4,
+            this.xinjian});
+            this.dataGridView2.Location = new System.Drawing.Point(-41, 227);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView2.Size = new System.Drawing.Size(543, 23);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column3";
+            this.Column4.Name = "Column4";
+            // 
+            // xinjian
+            // 
+            this.xinjian.HeaderText = "Column4";
+            this.xinjian.Name = "xinjian";
+            this.xinjian.ReadOnly = true;
+            this.xinjian.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.xinjian.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(303, 228);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(96, 19);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 8, 25, 0, 0, 0, 0);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "image.png");
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(444, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipTitle = "时间提醒";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 251);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+        private System.Windows.Forms.DataGridViewImageColumn Alarm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Detail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EndTime;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Finish;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewButtonColumn xinjian;
+    }
+}
+
---
+++ 04.代码/TomatoNotes/Notes/ToDoItemFile.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Notes
+{
+    class ToDoItemFile
+    {
+        private string _path = "D://Note.xml";
+        //public Item[] _items;
+       
+        public Item[] GetXml()
+        {
+            //读取Xml文件数据
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(_path);
+            //if (!File.Exists(_path))
+            //{
+            //    File.Create(_path);//如果路径下没有文件则创建文件
+            //}
+
+            XmlNodeList xmlNodeList = xmlDocument.SelectSingleNode("Notes").ChildNodes;
+            //dataGridView1.Rows.Clear();
+            int count = xmlNodeList.Count;
+            Item[] items = new Item[count];
+            int i = 0;
+            foreach (XmlNode xmlNode in xmlNodeList)
+            {
+                Item item = new Item();
+                XmlElement xmlElement = (XmlElement)xmlNode;
+                item._isAlarm = Convert.ToBoolean(xmlElement.ChildNodes.Item(0).InnerText);
+                item._detail = xmlElement.ChildNodes.Item(1).InnerText;
+                item._endTime = Convert.ToDateTime(xmlElement.ChildNodes.Item(2).InnerText);
+                item._isFinish = Convert.ToBoolean(xmlElement.ChildNodes.Item(3).InnerText);
+                item._id = Convert.ToInt32(xmlElement.ChildNodes.Item(4).InnerText);
+                items[i] = item; 
+                i++;
+            } 
+            return items;
+        }
+
+        public void WriteXml(Item newitem)
+        {
+            ////添加元素 things
+            XmlDocument myDoc = new XmlDocument();
+            myDoc.Load(_path);
+            XmlElement ele = myDoc.CreateElement("alarm");
+            XmlText text = myDoc.CreateTextNode(Convert.ToString(newitem._isAlarm));
+            XmlElement ele1 = myDoc.CreateElement("things");
+            XmlText text1 = myDoc.CreateTextNode(newitem._detail);
+            XmlElement ele2 = myDoc.CreateElement("time");
+            XmlText text2 = myDoc.CreateTextNode(Convert.ToString(newitem._endTime));
+            XmlElement ele3 = myDoc.CreateElement("finish");
+            XmlText text3 = myDoc.CreateTextNode(Convert.ToString(newitem._isFinish));
+            XmlElement ele4 = myDoc.CreateElement("id");
+            XmlText text4 = myDoc.CreateTextNode(Convert.ToString(newitem._id));
+            //添加节点
+            XmlNode newElem = myDoc.CreateNode("element", "Note", "");
+
+            //在节点添加元素
+            newElem.AppendChild(ele);
+            newElem.LastChild.AppendChild(text);
+            newElem.AppendChild(ele1);
+            newElem.LastChild.AppendChild(text1);
+            newElem.AppendChild(ele2);
+            newElem.LastChild.AppendChild(text2);
+            newElem.AppendChild(ele3);
+            newElem.LastChild.AppendChild(text3);
+            newElem.AppendChild(ele4);
+            newElem.LastChild.AppendChild(text4);
+
+            XmlElement root = myDoc.DocumentElement;
+            root.AppendChild(newElem);
+
+            //保存
+            myDoc.Save(_path);
+        }
+
+        public void UpdateXml(Item[] _items)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(_path);
+            XmlNode xmlElement_Notes = xmlDocument.SelectSingleNode("Notes");
+            xmlElement_Notes.RemoveAll();
+            foreach (Item it in _items)
+            {
+                XmlElement xmlElement_Note = xmlDocument.CreateElement("Note");
+                XmlElement xmlElement_alarm = xmlDocument.CreateElement("alarm");
+                xmlElement_alarm.InnerText = Convert.ToString(it._isAlarm);
+                xmlElement_Note.AppendChild(xmlElement_alarm);
+                //添加detail
+                XmlElement xmlElement_detail = xmlDocument.CreateElement("detail");
+                xmlElement_detail.InnerText = it._detail;
+                xmlElement_Note.AppendChild(xmlElement_detail);
+                //
+                XmlElement xmlElement_endtime = xmlDocument.CreateElement("endtime");
+                xmlElement_endtime.InnerText = Convert.ToString(it._endTime);
+                xmlElement_Note.AppendChild(xmlElement_endtime);
+                //
+                XmlElement xmlElement_finish = xmlDocument.CreateElement("finish");
+                xmlElement_finish.InnerText = Convert.ToString(it._isFinish);
+                xmlElement_Note.AppendChild(xmlElement_finish);
+                //
+                XmlElement xmlElement_id = xmlDocument.CreateElement("id");
+                xmlElement_id.InnerText = Convert.ToString(it._id);
+                xmlElement_Note.AppendChild(xmlElement_id);
+                xmlElement_Notes.AppendChild(xmlElement_Note);
+            }
+            
+            
+            xmlDocument.Save(_path);
+        }
+    }
+}
