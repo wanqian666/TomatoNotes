66d1fe0ac87791df418f4a5fcc65ad19d234b784
wanqian666 <qwe9812321qwe@sina.com>
2017-08-16 13:21:53 +0800

    Revert "Revert "2""
    This reverts commit 869cb87f7cf6008a5b24f911196d1cd6cd8471ae.

---
+++ 04.代码/TomatoNotes/Notes/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Notes
+{
+    class Item
+    {
+        public string _detail;
+        public DateTime _endTime;
+        public bool _isAlarm;
+        public bool _isFinish;
+        public int _id;
+    }
+
+}
---
+++ 04.代码/TomatoNotes/Notes/ToDoItemOperation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    class ToDoItemOperation
+    {
+        ToDoItemFile itemFile = new ToDoItemFile();
+        public Item[] items;
+        public Item[] items_self;
+        //public Item[] items_show;
+        //public int[] intCounts;
+        public void AddItem(Item newItem)
+        {
+            int count = items.Length;
+            items_self = new Item[count + 1];
+            items.CopyTo(items_self, 0);
+            items_self[count] = newItem;
+            items = items_self;
+            itemFile.WriteXml(items[count]);
+        }
+
+        public Item DeleteItem(Item newItem)
+        {
+            Item item = new Item();
+            item._detail = newItem._detail;
+            item._endTime = newItem._endTime;
+            item._isAlarm = newItem._isAlarm;
+            item._isFinish = newItem._isFinish;
+            return newItem;
+        }
+
+        public void ChangeItem(Item newItem, int num)
+        {
+            items[num] = newItem;
+            itemFile.UpdateXml(items);
+        }
+
+        public Item[] SearchItem()
+        {
+            //int num = 0, num2 = 0;
+            items = itemFile.GetXml();
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    if (!items[i]._isFinish)
+            //    {
+            //        num++;
+            //    }
+            //}
+            //items_show = new Item[num];
+            //intCounts = new int[num];
+            //for (int j = 0; j < num; j++)
+            //{
+            //    for (int i = num2; i < items.Length; i++)
+            //    {
+            //        if (!items[i]._isFinish)
+            //        {
+            //            items_show[j] = items[i];
+
+            //            //positions[j]._position = num2;
+            //            //positions[j]._item = new1;
+            //            intCounts[j] = num2;
+            //            num2 = i+1;    
+            //            break;
+            //        }
+            //    }
+            //}
+            return items;
+        }
+    }
+}
